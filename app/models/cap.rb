4386adf1c46c083e280a6110d1a9df314a728eb6
MacBook <macbook@MacBook-de-MacBook-2.local>
2011-05-22 13:51:30 -0300

    added to pagination

--- app/models/cap.rb
+++ app/models/cap.rb
@@ -1,7 +1,7 @@
 class Cap < ActiveRecord::Base
 
 def self.search(search,page,language)
-  paginate :per_page=>10, :page=>page,
+  paginate :per_page=>5, :page=>page,
     :conditions => [language + ' like ? and eng != ? and spa != ? and spa!=eng', "%#{search}%", "",""],
     :order  => 'num'
   end
