84a89822b91d8384919a6a7a75a7aae3c2d8673a
MacBook <macbook@MacBook-de-MacBook-2.local>
2011-06-09 01:27:20 -0300

    test

--- app/models/cap.rb
+++ app/models/cap.rb
@@ -41,7 +41,7 @@ def self.search(search,page,language)
   else
     Search.create(:search=>search, :lang=>language, :page=> page)
      paginate :per_page=>8, :page=>page,
-      :conditions => [language + ' similar to ?',"%#{search}(,|\.|\!|\?)%"],
+      :conditions => [language + ' similar to ?',"%#{search}(,|\.|\!|\?|\s+)%"],
       :order  => 'wcount'
 
     end
