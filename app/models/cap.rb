6d6ca61b5ef72ceb0e359115e9bea31f5adcb0ec
MacBook <macbook@MacBook-de-MacBook-2.local>
2011-06-09 01:17:12 -0300

    test

--- app/models/cap.rb
+++ app/models/cap.rb
@@ -43,7 +43,7 @@ def self.search(search,page,language)
     #   :conditions => ['('+language + ' like ? or ' + language + ' like ? or ' + language + ' like ?) and eng != ? and spa != ? and hide is not TRUE and spa!=eng', "% #{search} %","#{search} %","% #{search}", "",""],
     #   :order  => 'wcount'
     paginate :per_page=>8, :page=>page,
-      :conditions => [language + " like '#{search}'"],
+      :conditions => [language + " like '%#{search}%'"],
       :order  => 'wcount'
 
     end
