ed63bc7d00bca02915c61cca0bb9e6e61ad1f1a8
MacBook <macbook@MacBook-de-MacBook-2.local>
2011-06-10 22:12:09 -0300

    test

--- app/models/cap.rb
+++ app/models/cap.rb
@@ -41,7 +41,7 @@ def self.search(search,page,language)
   else
     Search.create(:search=>search, :lang=>language, :page=> page)
      paginate :per_page=>8, :page=>page,
-      :conditions => [language + ' similar to ? and similar to ? and similar to ? and hide !=1 and spa!=eng',"%^#{search}(,|\.|\!|\?|\s+)", "% #{search}(,|\.|\!|\?|\s+)$%", "% #{search}(,|\.|\!|\?|\s+)?%"],
+      :conditions => [language + ' similar to ? and hide !=1 and spa!=eng',"%#{search}(,|\.|\!|\?|\s+)%"],
       :order  => 'wcount'
 
     end
