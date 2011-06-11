bbca65ebf1860c8fe60578cd69b6616fe657bca9
MacBook <macbook@MacBook-de-MacBook-2.local>
2011-06-11 15:38:50 -0300

    test

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,6 +1,6 @@
 class TracksController < ApplicationController
   def index
-   @tracks= Track.paginate :page => params[:page], :order => 'id'
+   @tracks= Track.paginate :page => params[:page], :order => 'created_at desc'
    
   end
 
