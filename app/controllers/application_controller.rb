c3b1de7982aae8e4672413cb6ddde6bfcb61fc8f
mikefab <mikefabrikant@gmail.com>
2011-06-22 07:13:39 -0300

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   end
   
   def conj_verb
-  #  Rails.cache.clear() 
+   Rails.cache.clear() 
     if  Rails.cache.read("hash_verb") then
       return true
     else
