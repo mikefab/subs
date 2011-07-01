085658366380900245c3a1c0e75153b93329a262
mikefab <mikefabrikant@gmail.com>
2011-07-01 09:11:17 -0300

    test

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,8 +1,9 @@
 class TracksController < ApplicationController
   before_filter :authenticate
   def index
-    render :layout => false
+ 
    @tracks= Track.paginate :page => params[:page], :order => 'created_at desc'
+   
   end
 
   def clear
