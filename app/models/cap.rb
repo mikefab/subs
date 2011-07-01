3a42187a5a15728e09adc5c8ac7517dc2d58dea4
mikefab <mikefabrikant@gmail.com>
2011-07-01 04:19:55 -0300

    test

--- app/models/cap.rb
+++ app/models/cap.rb
@@ -34,10 +34,9 @@ def self.trash()
 end
 
 def self.return_movie(movie,page)
-  
     Search.create(:search=>movie,:page=> page)
     paginate :per_page=>4, :page=>page,
-    :conditions => ["source2=?","#{movie}"],
+    :conditions => ["source2=?","#{movie}"]
 end
 
 
